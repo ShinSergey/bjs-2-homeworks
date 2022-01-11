94188dd0f39a96113ac7b5374b6cc343ac897134
IvorWithCrossbow <sergey.shin.t@gmail.com>
2022-01-11 11:34:57 +0600

    1st task fix

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,16 +1,16 @@
 function compareArrays(arr1, arr2) {
-
-  if (arr1.length === arr2.length) {
-    return arr1.every(isEqual => isEqual === arr2);
+  
+  if (arr1.length === arr2.length && arr1.every(callback => callback === arr2[index])) {
+    return true;
   } else {
     return false;
   }
 }
 
-function advancedFilter(arr) {
-  let resultArr;
+  function advancedFilter(arr) {
+    let resultArr;
 
-  resultArr = arr.filter(isPositive => isPositive >= 1).filter(multiple3 => multiple3 % 3 === 0).map(x10 => x10 * 10);
+    resultArr = arr.filter(isPositive => isPositive >= 1).filter(multiple3 => multiple3 % 3 === 0).map(x10 => x10 * 10);
 
-  return resultArr; // array
-}
+    return resultArr;
+  }
